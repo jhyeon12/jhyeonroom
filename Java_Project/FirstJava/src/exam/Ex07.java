dcf05e4063950703c54f27a6e4c1668253b7c11e
jhyeon12 <wngusdl9328@gmail.com>
2021-10-14 17:35:42 +0900

    Update Ex07.java

--- Java_Project/FirstJava/src/exam/Ex07.java
+++ Java_Project/FirstJava/src/exam/Ex07.java
@@ -6,18 +6,6 @@ public class Ex07 {
 //	int형 1차원 배열을 매개변수로 전달 받아서 배열에 저장된 최대값,
 //
 //	그리고 최소값을 구해서 반환하는 메소드를 다음의 형태로 각각 정의.
-     public static void main(String[] args) {
-  	 
-  	 int[] arr = {2, 5, 314, 53, 21, 67, 233};
-  	 
-  	 System.out.println("최대값 : " + maxValue(arr));
-  	 System.out.println("최소값 : " + miniValue(arr));
-  	 
-  	 
-  	 
-   }
-//	​
-//
 //	public static int miniValue(int[] arr) { . . . . } // 최소값 반환
      public static int miniValue(int[] arr) {
 		int minValue=arr[0];
@@ -44,9 +32,6 @@ public class Ex07 {
 		return maxValue;
 	}
 	
-	
-//	​
-//
 //	위의 두 매소드는 인자로 전달되는 배열의 길이에 상관없이 동작하도록 정의.
 //
 //	두 매소드 실행을 확인인하기 위한 main 메소드 정의.
@@ -54,5 +39,17 @@ public class Ex07 {
 //	int형 배열에 채워질 정수는 프로그램 사용자로부터 입력 받고, 배열의 길이는
 //
 //	임의로 결정
+	public static void main(String[] args) {
+	  	 
+	  	 int[] arr = {2, 5, 314, 53, 21, 67, 233};
+	  	 
+	  	 System.out.println("최대값 : " + maxValue(arr));
+	  	 System.out.println("최소값 : " + miniValue(arr));
+	  	 
+	  	 
+	  	 
+	   }
+	
+
 	
 }

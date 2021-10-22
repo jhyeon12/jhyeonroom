9f20946c1c420da0d0cf23381b659ccfe47f43c2
jhyeon12 <wngusdl9328@gmail.com>
2021-10-22 18:26:44 +0900

    day1021

---
+++ Java_Project/FirstJava/src/chapter11/TreeSetTest2.java
@@ -0,0 +1,29 @@
+package chapter11;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.Set;
+import java.util.HashMap;
+
+
+
+
+public class TreeSetTest2 {
+
+	public static void main(String[] args) {
+		
+		TreeSet<Person> set = new TreeSet<Person>();
+		set.add(new Person("SON", 28));
+		set.add(new Person("LEE", 20));
+		set.add(new Person("Park", 40));
+		set.add(new Person("KIM", 40));
+		
+		Iterator<Person> itr = set.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		
+
+	}
+
+}

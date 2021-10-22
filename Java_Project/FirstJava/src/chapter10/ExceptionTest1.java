5df02c4941dab2b477a0a96121f1d7b77c399eff
jhyeon12 <wngusdl9328@gmail.com>
2021-10-22 18:24:43 +0900

    day1020

---
+++ Java_Project/FirstJava/src/chapter10/ExceptionTest1.java
@@ -0,0 +1,26 @@
+package chapter10;
+
+import java.util.Scanner;
+
+public class ExceptionTest1 {
+
+	public static void main(String[] args) {
+	
+Scanner sc = new Scanner(System.in);
+		
+		System.out.println("피제수를 입력하세요");
+		int num1 = sc.nextInt();
+		System.out.println("제수를 입력하세요");
+		int num2 = sc.nextInt();
+		
+		if(num2 == 0) {
+			System.out.println("0으로 나눌수 없습니다.");
+		} else {
+			
+		
+		
+		System.out.println(num1/num2);
+		}
+	}
+
+}
---
+++ Java_Project/FirstJava/src/exam/BadinputException.java
@@ -0,0 +1,8 @@
+package exam;
+
+public class BadinputException extends Exception {
+   
+	public BadinputException(String message) {
+		super("입력하신 아이디" + message + "에 영문자와 숫자 이외는 문자가 포함되어 있습니다.");
+	}
+}

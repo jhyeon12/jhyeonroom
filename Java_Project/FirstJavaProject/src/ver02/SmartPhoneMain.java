a0bb6041db3776e4d6108e9c8d2c3594eb7a7a6f
jhyeon12 <wngusdl9328@gmail.com>
2021-10-22 18:25:38 +0900

    day1022

---
+++ Java_Project/FirstJavaProject/src/ver02/SmartPhoneMain.java
@@ -0,0 +1,29 @@
+package ver02;
+
+public class SmartPhoneMain {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// 1.SmartPhone의 클래스의 인스턴스를 생성합니다.
+		SmartPhone phone = new SmartPhone(10);
+		// 2. 사용자로부터 입력을 받아 Contact 인스턴스를 생성해서 SmartPhone
+		// 3. 10번 반복해서 배열에 추가합니다.
+		for(int i=0; i<3; i++) {
+		phone.insertContact();
+		}
+		
+		// 4. 배열의 모든 요소를 출력합니다.
+		phone.printAllData();
+		// 5. 배열의 모든 요소를 검색합니다.
+		phone.searchPrint();
+		// 6. 배열의 요소를 삭제해 봅시다.
+		phone.deleteContact();
+        // 7. 배열의 요소를 수정해 봅시다.
+        phone.editContact();
+        // 삭제와 변경 여부 확인
+        phone.printAllData();
+	}
+
+}
